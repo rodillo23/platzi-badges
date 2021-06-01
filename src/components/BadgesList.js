2deafa8c66e71273eb588e2c2b5f776337ae9f8b
Rodolfo Figueroa <ficr23@hotmail.com>
2021-05-31 23:15:39 -0500

    badges list terminada

---
+++ src/components/BadgesList.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './styles/BadgesList.css'
+
+class BadgesList extends React.Component{
+  render(){
+    return(
+      <ul className="list-unstyled">
+        {
+          this.props.badges.map( badge => {
+            return (
+            <li key={badge.id}>
+              <div className="card__container">
+                <div className="badge__container-img">
+                  <img className="badge__img" src={badge.avatarUrl} alt="Avatar image"/>
+                </div>
+
+                <div className="badge__container-details">
+                  <p className="badge-name">{badge.firstName} {badge.lastName}</p>
+                  <p className="badge-twitter">@{badge.twitter}</p>
+                  <p className="badge-job">{badge.jobTitle}</p>
+                </div>
+              </div>
+            </li>)
+          })
+        }
+      </ul>
+    )
+  }
+}
+
+export default BadgesList
